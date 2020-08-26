11baea7b75c80244c1cda2ca67e01667a5bd33ae
Allen Xavier Arasan <xavierallem1999@gmail.com>
2020-08-26 18:09:32 +0200

    Update problem4.c

--- assignments/practice/problem4.c
+++ assignments/practice/problem4.c
@@ -45,26 +45,32 @@ int searchInFile(char *fname, char *str) {
     FILE *fp;
     int line_num = 1;
     int find_result = 0;
-    char temp[512];
+    //Changed size to be equal
+    int n=strlen(str);
+    char temp[n];
     
 
     if((fp = fopen(fname, "r")) == NULL) {
         printf("Error: Opening the file.\n");
+        delete [] temp;
       return(-1);
     }
 
-    while(fgets(temp, 512, fp) != NULL) {
+    while(fgets(temp, n, fp) != NULL) {
         if((strstr(temp, str)) != NULL) {
             printf("A match found on line: %d\n", line_num);
             printf("\n%s\n", temp);
             find_result++;
+            delete [] temp;
             return (1);
         }
         line_num++;
     }
+    delete [] temp;
 
     if(find_result == 0) {
         printf("\nSorry, couldn't find a match.\n");
+        
     }
     
     //Close the file if still open.
